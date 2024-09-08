65b63de359897e9dd9b10764e2594beaeee5bbcd
guarneerFPS <yyagci.98@gmail.com>
2024-09-08 13:03:07 +0200

    Fix abs path in VideoProcessing

--- MediaProcessor/src/VideoProcessing.cpp
+++ MediaProcessor/src/VideoProcessing.cpp
@@ -1,24 +1,34 @@
 #include "VideoProcessing.h"
+#include "Utils.h"
 #include <iostream>
-#include <cstdlib>
 #include <filesystem>
 #include <string>
 
 bool VideoProcessing::mergeAudioVideo(const std::string &videoPath, const std::string &audioPath, const std::string &outputPath) {
     
-    // FIXME: fix hardcoded paths that were used for testing
+    json config = Utils::loadConfig("config.json");
+    if (config.is_null()) {
+        std::cerr << "Error: Could not load configuration." << std::endl;
+        return false;
+    }
+
+    std::string ffmpegPath = config["ffmpeg_path"];
     std::string absoluteVideoPath = std::filesystem::absolute(videoPath).string();
     std::string absoluteAudioPath = std::filesystem::absolute(audioPath).string();
     std::string absoluteOutputPath = std::filesystem::absolute(outputPath).string();
 
+    // Remove the output file if it already exists (avoid interactive FFmpeg prompt)
+    Utils::removeFileIfExists(absoluteOutputPath);
+
     std::cout << "Merging video and audio..." << std::endl;
 
-    std::string ffmpegCommand = "ffmpeg -i \"" + absoluteVideoPath + "\" -i \"" + absoluteAudioPath + "\" -c:v copy -map 0:v:0 -map 1:a:0 -shortest \"" + absoluteOutputPath + "\"";
+    // Prepare ffmpeg command (-y flag to overwrite the file)
+    std::string ffmpegCommand = ffmpegPath + " -y -i \"" + absoluteVideoPath + "\" -i \"" + absoluteAudioPath + "\" -c:v copy -map 0:v:0 -map 1:a:0 -shortest \"" + absoluteOutputPath + "\"";
 
     std::cout << "Running FFmpeg command: " << ffmpegCommand << std::endl;
+    bool success = Utils::runCommand(ffmpegCommand);
 
-    int result = system(ffmpegCommand.c_str());
-    if (result != 0) {
+    if (!success) {
         std::cerr << "Error: Failed to merge audio and video using FFmpeg." << std::endl;
         return false;
     }
