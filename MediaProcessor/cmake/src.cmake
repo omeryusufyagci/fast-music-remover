287ab971cb5390a62315705f21ef2912167f5265
omeryusufyagci <omeryusufyagci@gmail.com>
2024-10-26 22:04:43 +0200

    Add Windows dynamic library and update src.cmake
    libdf.dll.a for linking and df.dll for runtime were added
    src.cmake file update to check for platform for linking DeepFilter

--- MediaProcessor/cmake/src.cmake
+++ MediaProcessor/cmake/src.cmake
@@ -13,9 +13,21 @@ add_executable(MediaProcessor
 )
 
 target_link_libraries(MediaProcessor PRIVATE Threads::Threads)
-target_link_libraries(MediaProcessor PRIVATE ${CMAKE_SOURCE_DIR}/lib/libdf.so)
 target_link_libraries(MediaProcessor PRIVATE ${SNDFILE_LIBRARIES})
 
+if(APPLE)
+    if(CMAKE_OSX_ARCHITECTURES MATCHES "arm64")
+        set(DF_LIBRARY ${CMAKE_SOURCE_DIR}/lib/aarch64-apple-darwin/libdf.dylib)
+    else()
+        set(DF_LIBRARY ${CMAKE_SOURCE_DIR}/lib/x86_64-apple-darwin/libdf.dylib)
+    endif()
+elseif(WIN32)
+    set(DF_LIBRARY ${CMAKE_SOURCE_DIR}/lib/x86_64-pc-windows-gnu/libdf.dll.a) # link with .dll.a, use .dll at runtime
+elseif(UNIX)
+    set(DF_LIBRARY ${CMAKE_SOURCE_DIR}/lib/x86_64-unknown-linux-gnu/libdf.so)
+else()
+    message(FATAL_ERROR "Unsupported platform")
+endif()
 set_target_properties(MediaProcessor PROPERTIES
     RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}"
 )
