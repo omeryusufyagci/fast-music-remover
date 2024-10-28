e6ffa9b06f51a5d84f6207aa8a0b818fab315675
omeryusufyagci <omeryusufyagci@gmail.com>
2024-10-28 21:44:09 +0100

    Fix UNIX .so link problem

--- MediaProcessor/cmake/src.cmake
+++ MediaProcessor/cmake/src.cmake
@@ -12,9 +12,7 @@ add_executable(MediaProcessor
     ${CMAKE_SOURCE_DIR}/src/DeepFilterCommandBuilder.cpp
 )
 
-target_link_libraries(MediaProcessor PRIVATE Threads::Threads)
-target_link_libraries(MediaProcessor PRIVATE ${SNDFILE_LIBRARIES})
-
+# Link DeepFilter wrt platform
 if(APPLE)
     if(CMAKE_OSX_ARCHITECTURES MATCHES "arm64")
         set(DF_LIBRARY ${CMAKE_SOURCE_DIR}/lib/libdf.dylib)
@@ -23,12 +21,20 @@ if(APPLE)
         # set(DF_LIBRARY ${CMAKE_SOURCE_DIR}/lib/libdf.dylib)
     endif()
 elseif(WIN32)
-    set(DF_LIBRARY ${CMAKE_SOURCE_DIR}/lib/libdf.dll.a) # link with .dll.a, use .dll at runtime
+    set(DF_LIBRARY ${CMAKE_SOURCE_DIR}/lib/libdf.dll.a) # .dll.a for linking, .dll for runtime
 elseif(UNIX)
     set(DF_LIBRARY ${CMAKE_SOURCE_DIR}/lib/libdf.so)
 else()
     message(FATAL_ERROR "Unsupported platform")
 endif()
+
+# Link the executable with necessary libraries
+target_link_libraries(MediaProcessor PRIVATE
+    Threads::Threads
+    ${SNDFILE_LIBRARIES}
+    ${DF_LIBRARY}
+)
+
 set_target_properties(MediaProcessor PROPERTIES
     RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}"
 )
