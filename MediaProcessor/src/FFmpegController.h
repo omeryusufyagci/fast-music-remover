fbff7c66849c72ac7c8e0f68b396dafcdba77385
Andrew Scott <git@andyscott.me>
2024-10-29 18:08:47 -0400

    Feature: Add `FFmpegController`
    Added the `FFmpegController` class with methods for `extractAudio()`,
    `generateChunkFile`, `mergeChunks()`, and `mergeMedia()`.
    Still TODO:
    1) doctrings for each method
    2) a test suite

---
+++ MediaProcessor/src/FFmpegController.h
@@ -0,0 +1,66 @@
+#ifndef FFMPEGCONTROLLER_H
+#define FFMPEGCONTROLLER_H
+
+#include <vector>
+
+#include "FFmpegCommandBuilder.h"
+#include "FFmpegConfigManager.h"
+
+namespace MediaProcessor {
+
+/**
+ * @brief An interface to orchestrate FFmpeg operations
+ */
+class FFmpegController {
+   public:
+    /**
+     * @brief Constructs a FFmpegController
+     *
+     * This constructor instantiates FFmpegController and initializes it with
+     * the given reference to a FFmpegConfigManager object.
+     *
+     * @param ffmpegConfig A reference to an instance of FFmpegConfigManager
+     */
+    FFmpegController(const FFmpegConfigManager& ffmpegConfig);
+
+    /**
+     * @brief Extracts the audio with FFmpeg
+     *
+     * @return True if successful, otherwise false
+     */
+    bool extractAudio();
+
+    /**
+     * @brief Generates the chunk file with FFmpeg
+     *
+     * @return True if successful, otherwise false
+     */
+    std::vector<fs::path> splitMedia(int numChunks, double overlapDuration);
+
+    /**
+     * @brief Merges media chunks with FFmpeg
+     *
+     * @return True if successful, otherwise false
+     */
+    bool mergeChunks(const std::vector<fs::path>& processedChunkCol, double overlapDuration);
+
+    /**
+     * @brief Merges media files with FFmpeg
+     *
+     * @return True if successful, otherwise false
+     */
+    bool mergeMedia(const fs::path& videoPath, const fs::path& audioPath);
+
+   private:
+    const FFmpegConfigManager& m_ffmpegConfig;
+    FFmpegCommandBuilder m_ffmpegCmdBuilder;
+
+    void populateChunkDurations(std::vector<double>& startTimes, std::vector<double>& durations,
+                                int numChunks, double overlapDuration);
+    bool generateChunkFile(const int chunkIndex, const double startTime, const double duration,
+                           std::vector<fs::path>& chunkPathCol);
+};
+
+}  // namespace MediaProcessor
+
+#endif /* FFMPEGCONTROLLER_H */
